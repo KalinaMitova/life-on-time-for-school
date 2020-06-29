b47bb526047fe0e765b917906891eb384b6d3ccf
KalinaMitova <kalina.mitova@gmail.com>
2020-06-29 20:59:41 +0300

    add prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,20 @@
 //apiUrl: 'http://lotapi.cweb.bg/', https://lifeontime.co.uk/api
 // fileUplodeUrl: ' http://lotapp.cweb.bg/uploads/'
 
+////for the original app
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://lifeontime.co.uk/api/',
+//   fileUplodeUrl: 'https://lifeontime.co.uk/app/uploads/',
+//   wp_url: 'https://lifeontime.co.uk/',
+//   google_calendar__url: 'https://lifeontime.co.uk/app/sync/',
+// };
+
+//for the cloned app2
 export const environment = {
   production: true,
-  apiUrl: 'https://lifeontime.co.uk/api/',
-  fileUplodeUrl: 'https://lifeontime.co.uk/app/uploads/',
+  apiUrl: 'https://lifeontime.co.uk/api2/',
+  fileUplodeUrl: 'https://lifeontime.co.uk/app2/uploads/',
   wp_url: 'https://lifeontime.co.uk/',
-  google_calendar__url: 'https://lifeontime.co.uk/app/sync/',
+  google_calendar__url: 'https://lifeontime.co.uk/app2/sync/',
 };
